b2c5680fe46e92642223d1524b1b5d250a178996
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2018-06-30 20:50:09 +0100

    progress

--- MonzoBalance/AppDelegate.swift
+++ MonzoBalance/AppDelegate.swift
@@ -17,12 +17,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     // GET THESE CREDENTIALS FROM DEVELOPERS.MONZO.COM
     // DO NOT PUBLISH THEM IN PUBLIC.
-    let accessToken = "ACCESS TOKEN HERE"
-    let account = "ACCOUNT NUMBER HERE"
+    var credentials = MonzoCredentials(accountId: "ACCOUNT NUMBER HERE", accessToken: "ACCESS TOKEN HERE")
     // END
     
-    @objc func refreshAndDisplayBalances() {
-        monzoApi.fetchBalance(account: account, accessToken: accessToken) { (balance) in
+    func refreshAndDisplayBalances(credentials: MonzoCredentials) {
+        monzoApi.fetchBalance(account: credentials.accountId, accessToken: credentials.accessToken) { (balance) in
             DispatchQueue.main.async {
                 self.statusItem.title = balance[0]
                 if let spendToday = self.monzoMenu.item(withTag: 1) {
@@ -31,15 +30,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
         }
     }
+    
+    @objc func refreshBalances(){
+        refreshAndDisplayBalances(credentials: credentials)
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         statusItem.title = "Loading Balance"
         statusItem.menu = monzoMenu
         
-        refreshAndDisplayBalances()
+        refreshAndDisplayBalances(credentials: credentials)
         
-        _ = Timer.scheduledTimer(timeInterval: 30.0, target: self, selector: #selector(refreshAndDisplayBalances), userInfo: nil, repeats: true)
+        _ = Timer.scheduledTimer(timeInterval: 30.0, target: self, selector: #selector(refreshBalances), userInfo: nil, repeats: true)
         
     }
 
@@ -49,6 +52,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBAction func quitClicked(_ sender: NSMenuItem) {
         NSApplication.shared.terminate(self)
+    }
+    
+    @IBAction func enterCredentialsManually(_ sender: Any) {
+        let wc = CredentialsInputWindowController(windowNibName: NSNib.Name( "CredentialsInputWindow" ))
+        wc.showWindow(nil)
     }
     
     @IBAction func spentTodayClicked(_ sender: NSMenuItem) {
