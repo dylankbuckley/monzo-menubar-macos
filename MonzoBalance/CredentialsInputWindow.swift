d97747aec64f3ba9ac98e88a98b096d5a24ad58c
dylankbuckley <dbuckley14@icloud.com>
2018-07-01 15:32:51 +0100

    Allow Copy and Paste from Keyboard

--- MonzoBalance/CredentialsInputWindow.swift
+++ MonzoBalance/CredentialsInputWindow.swift
@@ -31,3 +31,27 @@ class CredentialsInputWindow: NSViewController {
     }
     
 }
+
+extension NSTextField {
+    override open func performKeyEquivalent(with event: NSEvent) -> Bool {
+        if event.type == NSEvent.EventType.keyDown {
+            if (event.modifierFlags.rawValue & NSEvent.ModifierFlags.deviceIndependentFlagsMask.rawValue) == NSEvent.ModifierFlags.command.rawValue {
+                switch event.charactersIgnoringModifiers! {
+                case "x":
+                    if NSApp.sendAction(#selector(NSText.cut(_:)), to:nil, from:self) { return true }
+                case "c":
+                    if NSApp.sendAction(#selector(NSText.copy(_:)), to:nil, from:self) { return true }
+                case "v":
+                    if NSApp.sendAction(#selector(NSText.paste(_:)), to:nil, from:self) { return true }
+                case "z":
+                    if NSApp.sendAction(Selector(("undo:")), to:nil, from:self) { return true }
+                case "a":
+                    if NSApp.sendAction(#selector(NSResponder.selectAll(_:)), to:nil, from:self) { return true }
+                default:
+                    break
+                }
+            }
+        }
+        return super.performKeyEquivalent(with: event)
+    }
+}
